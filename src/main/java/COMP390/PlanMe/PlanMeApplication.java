4a65cd68b5919ed26ed13882422c135bfcfe904c
Dawid Rybinski <42513264+dawid3201@users.noreply.github.com>
2023-06-13 17:11:11 +0100

    Initial commit

---
+++ src/main/java/COMP390/PlanMe/PlanMeApplication.java
@@ -0,0 +1,13 @@
+package COMP390.PlanMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/COMP390/PlanMe/PlanMeApplicationTests.java
@@ -0,0 +1,13 @@
+package COMP390.PlanMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
