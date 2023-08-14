60237518cfd3d3750161815e4d37c19ca69a516e
Dawid Rybinski <42513264+dawid3201@users.noreply.github.com>
2023-08-14 14:53:41 +0100

    I replaced drag and drop in vanila JS to JQuery sortable. Bars can be moved. CSS changes in styles.css Tried to implement WebSockets for in real time communication - TODO Drag.js replaced with dragBar.js and dragTask.js

--- src/main/java/COMP390/PlanMe/entity/Task.java
+++ src/main/java/COMP390/PlanMe/entity/Task.java
@@ -6,8 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Table(name = "tasks")
 @JsonIdentityInfo(
